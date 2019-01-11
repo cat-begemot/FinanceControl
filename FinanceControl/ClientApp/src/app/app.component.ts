f55ef07f6a5d52de0b1aa39395ad769cfe55af18
Alex Holenko <cat-begemot@live.com>
2019-01-11 17:59:36 +0200

    Fixed (deleted sessions support, approp configured identity, moved AddMvc() down

--- FinanceControl/ClientApp/src/app/app.component.ts
+++ FinanceControl/ClientApp/src/app/app.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './auth/authentication.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
-    public authService: AuthenticationService
+    public authService: AuthenticationService,
+    private routerNav: Router
   ) { }
 
   public click_Logout(){
     if(this.authService.authenticated==true){
       this.authService.logout();
+    }
+  }
+
+  ngOnInit(){
+    if(this.authService.authenticated==true){
+      this.routerNav.navigate(["/accounts"]);
     }
   }
 }
--- FinanceControl/Startup.cs
+++ FinanceControl/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace FinanceControl
 {
@@ -24,10 +25,10 @@ namespace FinanceControl
 		}
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc();
-
 			services.AddHttpContextAccessor();
-
+			
+			/*
+			// Add sessions support
 			services.AddDistributedSqlServerCache(options =>
 			{
 				options.ConnectionString=Configuration["ConnectionStrings:DefaultConnection"];
@@ -40,7 +41,9 @@ namespace FinanceControl
 				options.IdleTimeout = System.TimeSpan.FromDays(1);
 				options.Cookie.HttpOnly = false;
 			});
+			*/
 
+			// Setup connection strings
 			if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
 			{
 				services.AddDbContext<DbRepositoryContext>(options =>
@@ -64,11 +67,15 @@ namespace FinanceControl
 			services.BuildServiceProvider().GetService<DbRepositoryContext>().Database.Migrate();
 			services.BuildServiceProvider().GetService<IdentityDataContext>().Database.Migrate();
 
-
+			// Binding interface with it realization class
 			services.AddTransient<IRepository, Repository>();
 
 			// Identity configurations
-			services.AddIdentity<User, IdentityRole>(options=>
+
+
+			services.AddDefaultIdentity<User>().AddEntityFrameworkStores<IdentityDataContext>();
+
+			services.Configure<IdentityOptions>(options =>
 			{
 				options.Password.RequireDigit = false;
 				options.Password.RequiredLength = 3;
@@ -76,16 +83,26 @@ namespace FinanceControl
 				options.Password.RequireLowercase = false;
 				options.Password.RequireNonAlphanumeric = false;
 				options.Password.RequireUppercase = false;
-			}).AddEntityFrameworkStores<IdentityDataContext>();
-			
+			});
+
+			services.ConfigureApplicationCookie(options =>
+			{
+				options.Cookie.HttpOnly = true;
+			});
+
+			//services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
-			app.UseSession();
-			app.UseIdentity();
+			//app.UseSession();
 			app.UseStaticFiles();
+
+			app.UseAuthentication();
+
 			app.UseMvc(routes =>
 			{
 				routes.MapRoute(
